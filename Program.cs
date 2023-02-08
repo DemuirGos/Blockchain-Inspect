96da0f844620006c6eb71cd0fdf1d84c61ead005
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2023-02-08 21:55:50 +0100

    Add retries

--- Program.cs
+++ Program.cs
@@ -5,15 +5,17 @@ using System.Threading.Tasks;
 using Nethereum.BlockchainProcessing.BlockStorage.Entities;
 using Nethereum.Hex.HexConvertors.Extensions;
 using Nethereum.Hex.HexTypes;
+using Nethereum.JsonRpc.Client;
 using Nethereum.RPC.Eth.DTOs;
 using Nethereum.Web3;
 
 namespace NethereumSample
 {
+
     class Program
     {
         public static readonly byte[] EofPrefix = { 0xEF, 0x00 };
-        static Web3? web3 = new Nethereum.Web3.Web3("[<Node Ip and Stuff>]");
+        static Web3? web3 = new Nethereum.Web3.Web3("[<Node::Ip>]");
         static int BlockchainHeight = 19040000;
         static Stopwatch timer = new Stopwatch();
         static async Task Main(string[] args)
@@ -21,21 +23,28 @@ namespace NethereumSample
             bool StartWithEofPrefixTx(byte[] bytecode) => bytecode.AsSpan().StartsWith(EofPrefix);
             List<BlockWithTransactions> blocks = new(); 
             int batchSize= 1000;
-            int faulty = 0;
             int len = BlockchainHeight / batchSize;
-            var AnyEofsFound = await Task.WhenAll(Enumerable.Range(0, len)
-                .Select(i => (i * batchSize, (i + 1) * batchSize))
-                .Select((range) => IsThereAnyEofInBlock(range.Item1, range.Item2, StartWithEofPrefixTx)));
 
-            
+            var AnyEofsFound = false;
+            for(int i = 0; !AnyEofsFound && i < len; i++) {
+                var start = i * batchSize;
+                var end = (i + 1) * batchSize;
+                int retries = 10;
+                while(retries >= 0 && !AnyEofsFound) {
+                    try {
+                        AnyEofsFound = await IsThereAnyEofInBlock(start, end, StartWithEofPrefixTx);
+                    } catch {
+                        retries--;
+                    }
+                }
+            }
 
-            if(AnyEofsFound.Any()) {
+            if(AnyEofsFound) {
                 Console.WriteLine("EOF found");
             } else {
                 Console.WriteLine("No EOF found");
             }
         }
-
         static async Task<bool> IsThereAnyEofInBlock(BigInteger start, BigInteger end, Func<byte[], bool> Check) {
             Console.WriteLine("Handling batch: " + start + " - " + end);
             try {
@@ -52,15 +61,18 @@ namespace NethereumSample
                     var deployedContractsBytecode = await Task.WhenAll(
                         createTxReceipts
                             .Select(r => r.ContractAddress) // get the address of the deployed contract
+                            .Where(address => address != null) // filter out null addresses
                             .Select(address => web3.Eth.GetCode.SendRequestAsync(address)).ToArray()); // get the bytecode of the deployed contract
                     // check if the bytecode starts with the EOF prefix
                     var deployedContracts = deployedContractsBytecode
-                        .Select(hexCode =>hexCode.HexToByteArray()).Where(Check);
+                        .Select(hexCode => hexCode.HexToByteArray()).Where(Check);
                     if(deployedContracts.Any()) {
                         Console.WriteLine("Found EOF at block: " + i);
                         return true;
                     }
                 }
+            } catch(RpcClientTimeoutException e) {
+                throw;
             } catch(Exception e) {
                 Console.WriteLine("Error handling : " + e.Message + " batch" + start + " - " + end );
             } finally {
